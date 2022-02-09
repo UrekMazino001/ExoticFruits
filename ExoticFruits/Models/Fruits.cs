a03a2d34ffd746b873b24aae5a03c87ce1a88731
jacabrera <ariel.cabrera002@hotmail.com>
2022-02-09 13:27:47 -0600

    Add project files.

---
+++ ExoticFruits/Models/Fruits.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExoticFruits.Models
+{
+    public class Fruits
+    {
+        public string Picture { get; set; }
+        public string Title { get; set; }
+        public string Price { get; set; }
+        public string BGColor { get; set; }
+    }
+}
